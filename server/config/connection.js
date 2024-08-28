1521244c1c95ccfb8688c88653e1afee11045206
benjamin-parks <benparks93@gmail.com>
2024-08-28 15:10:11 -0500

    Added packages to get sequelize working with the server and postGresql

---
+++ server/config/connection.js
@@ -0,0 +1,21 @@
+// connect to a postgres database 
+const {Sequelize} = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres'
+    }
+  );
+}
+
+module.exports = sequelize;
