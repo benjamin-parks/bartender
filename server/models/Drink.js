82226c52c5b74978b4ac1b1764e1915e23b1726f
benjamin-parks <benparks93@gmail.com>
2024-08-29 16:30:45 -0500

    I think the models are created properly! Let me know if you think there's a better way to do this.

---
+++ server/models/Drink.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Drink extends Model {}
+
+Drink.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        drink_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        drink_ingredients: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        drink_instructions: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        drink_image: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        main_liquor: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    }
+    ,
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'drink',
+    }
+);
+
+module.exports = Drink;
+  
