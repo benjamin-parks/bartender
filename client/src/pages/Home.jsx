aef42e60029ce0f7131d13d898f0c4d0d482a860
Alex LaPierre <aslapierre7@gmail.com>
2024-09-03 13:58:11 -0500

    added routes, added cors, displayed info from pg to front end

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
+import Test from "../components/Test";
 
 export default function Home() {
     return (
         <>
             <Header />
             <h1>Bartender</h1>
+            <h2>-----TEST-----</h2>
+                <Test />
         </>
     )
 }
---
+++ server/controllers/userController.js
@@ -0,0 +1,12 @@
+const User = require('../models/User');
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.findAll();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+    };
+
+module.exports = { getAllUsers };
